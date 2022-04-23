9d3c370768284dbb866d40baf1cc9859a212acd7
Ofir Zeitoun <ofir.zeitoun@gmail.com>
2022-04-23 20:42:32 +0300

    fix async functions mapping

--- lib/utils/types.d.ts
+++ lib/utils/types.d.ts
@@ -5,7 +5,7 @@ export interface ActionPayload<T> {
     payload?: T[keyof T];
 }
 export declare type ActionsType<T> = Without<{
-    [Property in keyof T]: T[Property] extends (state: object) => void ? () => ActionPayload<T> : T[Property] extends (state: object, payload: any) => void ? (payload: Parameters<T[Property]>[1]) => ActionPayload<T> : never;
+    [Property in keyof T]: T[Property] extends (state: object) => void ? () => ActionPayload<T> : T[Property] extends (state: object, payload: any) => void ? (payload: Parameters<T[Property]>[1]) => ActionPayload<T> : T[Property] extends (dispatch: Function) => Promise<void> ? () => ActionPayload<T> : T[Property] extends (dispatch: Function, getState: Function, extraArgument: any, payload: infer PayloadType) => Promise<void> ? (payload: PayloadType) => ActionPayload<T> : never;
 }, never>;
 export declare type CallbacksType<T> = ExtractType<T, (...args: any[]) => void>;
 export declare type StateType<T> = ExcludeType<T, (...args: any[]) => void>;
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -18,6 +18,15 @@ export type ActionsType<T> = Without<
       ? () => ActionPayload<T> // no params
       : T[Property] extends (state: object, payload: any) => void
       ? (payload: Parameters<T[Property]>[1]) => ActionPayload<T> // with params
+      : T[Property] extends (dispatch: Function) => Promise<void>
+      ? () => ActionPayload<T> // no params
+      : T[Property] extends (
+          dispatch: Function,
+          getState: Function,
+          extraArgument: any,
+          payload: infer PayloadType
+        ) => Promise<void>
+      ? (payload: PayloadType) => ActionPayload<T>
       : never;
   },
   never
