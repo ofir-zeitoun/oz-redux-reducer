aa2a0ab18b07bc691750fd97b0272b7c788b25be
Ofir Zeitoun <ofir.zeitoun@gmail.com>
2022-05-22 00:54:48 +0300

    fix run time in build mode - can't tell if function is async

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,7 +1,17 @@
-const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
+const AsyncFunction = "AsyncFunction";
 
 export const identityFn = <T>(value: T) => value;
 
 export const isFunc = (func: any) => typeof func === "function";
 
-export const isAsync = (func: any) => func.constructor.name === AsyncFunction.name;
+const isAsyncCompiled =
+  /((\.(then|catch)\s*\()|(^async\s+)|(\s+new\s+Promise\s*\()|(\s+Promise\.)|(\s+await\s+))/;
+
+export const isAsync = (func: any) => {
+  // dev mode
+  if (func.constructor.name === AsyncFunction) {
+    return true;
+  }
+
+  return isAsyncCompiled.test(func.toString());
+};
