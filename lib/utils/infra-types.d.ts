c3375327f07b9a6cec8d847278a91be189f251f1
unknown <ofir.zeitoun@gmail.com>
2022-11-28 11:06:56 +0200

    fix typing, remove worng package-lock

--- lib/utils/infra-types.d.ts
+++ lib/utils/infra-types.d.ts
@@ -1,17 +1,17 @@
-export declare type Constructor<T = {}> = new (...args: any[]) => T;
-export declare type Without<T, V, WithNevers = {
+export type Constructor<T = {}> = new (...args: any[]) => T;
+export type Without<T, V, WithNevers = {
     [K in keyof T]: Exclude<T[K], undefined> extends V ? never : T[K] extends Record<string, unknown> ? Without<T[K], V> : T[K];
 }> = Pick<WithNevers, {
     [K in keyof WithNevers]: WithNevers[K] extends never ? never : K;
 }[keyof WithNevers]>;
-export declare type ExtractKeys<T, U> = {
+export type ExtractKeys<T, U> = {
     [Key in keyof T]: T[Key] extends U ? Key : never;
 }[keyof T] & keyof T;
-export declare type ExtractType<T, U> = Pick<T, ExtractKeys<T, U>>;
-export declare type ExcludeKeys<T, U> = {
+export type ExtractType<T, U> = Pick<T, ExtractKeys<T, U>>;
+export type ExcludeKeys<T, U> = {
     [Key in keyof T]: T[Key] extends U ? never : Key;
 }[keyof T] & keyof T;
-export declare type ExcludeType<T, U> = Pick<T, ExcludeKeys<T, U>>;
-export declare type Entry<T> = {
+export type ExcludeType<T, U> = Pick<T, ExcludeKeys<T, U>>;
+export type Entry<T> = {
     [K in keyof T]: [K, T[K]];
 }[keyof T];
--- lib/utils/types.d.ts
+++ lib/utils/types.d.ts
@@ -1,12 +1,12 @@
 import { ExcludeType, ExtractKeys, ExtractType, Without } from "./infra-types";
-export declare type ActionTypeKeys<T> = ExtractKeys<T, Function>;
+export type ActionTypeKeys<T> = ExtractKeys<T, Function>;
 export interface ActionPayload<T> {
     type: ActionTypeKeys<T>;
     payload?: T[keyof T];
 }
-export declare type CallbacksType<T> = ExtractType<T, (...args: any[]) => void>;
-export declare type StateType<T> = ExcludeType<T, (...args: any[]) => void>;
-export declare type GetState<T> = ReturnType<() => StateType<T>>;
+export type CallbacksType<T> = ExtractType<T, (...args: any[]) => void>;
+export type StateType<T> = ExcludeType<T, (...args: any[]) => void>;
+export type GetState<T> = ReturnType<() => StateType<T>>;
 interface IResetStateAction {
     resetState(): ActionPayload<void>;
 }
@@ -17,8 +17,8 @@ export declare class ResetState<T> implements IResetState<T> {
     resetState: () => StateType<T>;
     constructor(initialState: StateType<T>);
 }
-export declare type ActionsType<T> = Without<{
+export type ActionsType<T> = Without<{
     [Property in keyof T]: T[Property] extends (state: StateType<T>) => void ? () => ActionPayload<T> : T[Property] extends (state: StateType<T>, payload: any) => void ? (payload: Parameters<T[Property]>[1]) => ActionPayload<T> : T[Property] extends (dispatch: Function) => Promise<void> ? () => ActionPayload<T> : T[Property] extends (dispatch: Function, getState: GetState<T>) => Promise<void> ? () => ActionPayload<T> : T[Property] extends (dispatch: Function, getState: GetState<T>, extraArgument: any) => Promise<void> ? () => ActionPayload<T> : T[Property] extends (dispatch: Function, getState: GetState<T>, extraArgument: any, payload: infer PayloadType) => Promise<void> ? (payload: PayloadType) => ActionPayload<T> : never;
 }, never> & IResetStateAction;
-export declare type Reducer<T> = (state: StateType<T> | undefined, { type, payload }: ActionPayload<T>) => StateType<T>;
+export type Reducer<T> = (state: StateType<T> | undefined, { type, payload }: ActionPayload<T>) => StateType<T>;
 export {};
