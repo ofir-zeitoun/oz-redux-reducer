b34426993d255c372c17aa3acd37d94c721970e6
Ofir <ofir.zeitoun@gmail.com>
2023-09-18 21:35:31 +0300

    fix type dispatch

--- lib/utils/types.d.ts
+++ lib/utils/types.d.ts
@@ -1,3 +1,4 @@
+import { Dispatch } from "redux";
 import { ExcludeType, ExtractKeys, ExtractType, Without } from "./infra-types";
 export type ActionTypeKeys<T> = ExtractKeys<T, Function>;
 export interface ActionPayload<T> {
@@ -18,7 +19,7 @@ export declare class ResetState<T> implements IResetState<T> {
     constructor(initialState: StateType<T>);
 }
 export type ActionsType<T> = Without<{
-    [Property in keyof T]: T[Property] extends (state: StateType<T>) => void ? () => ActionPayload<T> : T[Property] extends (state: StateType<T>, payload: any) => void ? (payload: Parameters<T[Property]>[1]) => ActionPayload<T> : T[Property] extends (dispatch: Function) => Promise<void> ? () => ActionPayload<T> : T[Property] extends (dispatch: Function, getState: GetState<T>) => Promise<void> ? () => ActionPayload<T> : T[Property] extends (dispatch: Function, getState: GetState<T>, extraArgument: any) => Promise<void> ? () => ActionPayload<T> : T[Property] extends (dispatch: Function, getState: GetState<T>, extraArgument: any, payload: infer PayloadType) => Promise<void> ? (payload: PayloadType) => ActionPayload<T> : never;
+    [Property in keyof T]: T[Property] extends (state: StateType<T>) => void ? () => ActionPayload<T> : T[Property] extends (state: StateType<T>, payload: any) => void ? (payload: Parameters<T[Property]>[1]) => ActionPayload<T> : T[Property] extends (dispatch: Dispatch) => Promise<void> ? () => ActionPayload<T> : T[Property] extends (dispatch: Dispatch, getState: GetState<T>) => Promise<void> ? () => ActionPayload<T> : T[Property] extends (dispatch: Dispatch, getState: GetState<T>, extraArgument: any) => Promise<void> ? () => ActionPayload<T> : T[Property] extends (dispatch: Dispatch, getState: GetState<T>, extraArgument: any, payload: infer PayloadType) => Promise<void> ? (payload: PayloadType) => ActionPayload<T> : never;
 }, never> & IResetStateAction;
 export type Reducer<T> = (state: StateType<T> | undefined, { type, payload }: ActionPayload<T>) => StateType<T>;
 export {};
